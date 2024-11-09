6a3c71864348f0b5cec5cc1673825cc7bfd4b554
sangriel <39114237+sangriel@users.noreply.github.com>
2024-11-09 19:02:57 +0900

    add transport flow (#3)

--- MiniSuperApp/TransportHome/TransportHomeRouter.swift
+++ MiniSuperApp/TransportHome/TransportHomeRouter.swift
@@ -1,22 +1,40 @@
 import ModernRIBs
 
-protocol TransportHomeInteractable: Interactable {
-  var router: TransportHomeRouting? { get set }
-  var listener: TransportHomeListener? { get set }
+protocol TransportHomeInteractable: Interactable, TopupListener {
+    var router: TransportHomeRouting? { get set }
+    var listener: TransportHomeListener? { get set }
 }
 
 protocol TransportHomeViewControllable: ViewControllable {
-  // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+    // TODO: Declare methods the router invokes to manipulate the view hierarchy.
 }
 
 final class TransportHomeRouter: ViewableRouter<TransportHomeInteractable, TransportHomeViewControllable>, TransportHomeRouting {
-  
-  override init(
-    interactor: TransportHomeInteractable,
-    viewController: TransportHomeViewControllable
-  ) {
-    super.init(interactor: interactor, viewController: viewController)
-    interactor.router = self
-  }
-  
+    
+    private let topupBuilder : TopupBuildable
+    private var topupRouting : Routing?
+    
+    init(
+        interactor: TransportHomeInteractable,
+        viewController: TransportHomeViewControllable,
+        topupBuilder : TopupBuildable
+    ) {
+        self.topupBuilder = topupBuilder
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+}
+extension TransportHomeRouter {
+    func attachTopup() {
+        if topupRouting != nil { return }
+        let router = topupBuilder.build(withListener: interactor)
+        topupRouting = router
+        attachChild(router)
+    }
+    
+    func detachTopup() {
+        guard let topupRouting = self.topupRouting else { return }
+        detachChild(topupRouting)
+        self.topupRouting = nil
+    }
 }
